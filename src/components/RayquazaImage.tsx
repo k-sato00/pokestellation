bb37ca91a0b9b09dad392dea523fe31838e477d0
k-sato00 <k.saugucca1996@gmail.com>
2026-03-22 11:25:12 +0900

    Delete src directory

--- src/components/RayquazaImage.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const RayquazaImage: React.FC = () => (
-  <img src={`${process.env.PUBLIC_URL}/レックウザ.png`} alt="レックウザ" style={{ height: 140, marginLeft: 24 }} />
-);
-
-export default RayquazaImage;
